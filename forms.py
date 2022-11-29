363be354a6c2953f6b3fb2e8fa2f155611965507
merrymira <sarabangkok@gmail.com>
2022-11-29 21:17:45 +0700

    Initial Commit

---
+++ forms.py
@@ -0,0 +1,20 @@
+from flask import Flask, url_for, render_template
+from flask_wtf import FlaskForm
+from werkzeug.utils import redirect
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditorField, CKEditor
+
+
+##WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+
+class CommentForm(FlaskForm):
+    comment_text = CKEditorField("Comment", validators=[DataRequired()])
+    submit = SubmitField("Submit Comment")
